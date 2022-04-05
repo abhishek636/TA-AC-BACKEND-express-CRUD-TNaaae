9cd5a41e4f74dc386f5d6fe491af047b1b99c181
Abhishek Bhatt <bhattabhishek636@gmail.com>
2022-04-05 13:13:40 +0530

    block-BNaacv

---
+++ block-BNaacv/user-diary/app.js
@@ -0,0 +1,10 @@
+const express = require("express");
+const app = express();
+const path = require("path");
+let userRouter = require("./routes/users");
+app.set("view engine", "ejs");
+app.set("views", path.join(__dirname, "views"));
+app.use('/users' ,userRouter);
+app.listen(5000 , ()=>{
+    console.log('server is listening on the port 5K ');
+})
